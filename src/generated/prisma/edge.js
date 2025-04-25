ecded8ba67b127eacfc9bc4d9076658be029b86a
Matthew <mjbinsigne2022@plm.edu.ph>
2025-04-25 19:57:01 +0800

    Update package.json and generated/prisma

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -219,11 +219,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:root@localhost:5432/revault"
+        "value": null
       }
     }
   },
