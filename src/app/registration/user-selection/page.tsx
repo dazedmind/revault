fb959a02eafec88dda229fac031517033abb8c11
John Allen <dazedmind1@protonmail.com>
2025-05-22 16:14:38 +0800

    minor ui fixes

--- src/app/registration/user-selection/page.tsx
+++ src/app/registration/user-selection/page.tsx
@@ -10,46 +10,49 @@ const UserSelection = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-40">
-      <h1 className="text-4xl font-mono text-center font-bold text-gold">
-        Select what describes you
-      </h1>
+    <div className="flex flex-col my-20 md:my-40 align-middle justify-center items-center">
+      <div className="flex flex-col align-middle justify-center items-center">
+        <h1 className="text-4xl font-mono text-center font-bold text-gold">
+          Select what describes you
+        </h1>
 
-      <div className="flex md:flex-row flex-col justify-evenly gap-8 mt-10">
-        <button
-          onClick={handleUserTypeSelection.bind(null, "student")}
-          className={`w-xs h-48 bg-accent border-2 rounded-lg hover:bg-white hover:shadow-lg transition-all duration-300 flex flex-col justify-center items-center p-6 cursor-pointer`}
-        >
-          <PiStudentBold className="size-12 mb-2" />
+        <div className="flex md:flex-row flex-col justify-evenly gap-8 mt-10">
+          <button
+            onClick={handleUserTypeSelection.bind(null, "student")}
+            className={`w-xs h-48 bg-accent border-2 rounded-lg hover:bg-white hover:shadow-lg transition-all duration-300 flex flex-col justify-center items-center p-6 cursor-pointer`}
+          >
+            <PiStudentBold className="size-12 mb-2" />
 
-          {/* Main heading */}
-          <p className="text-xl font-bold text-center text-gold leading-tight mb-1">
-            I&apos;m a student
-          </p>
+            {/* Main heading */}
+            <p className="text-xl font-bold text-center text-gold leading-tight mb-1">
+              I&apos;m a student
+            </p>
 
-          {/* Sub-text */}
-          <p className="text-sm text-center w-[244px] leading-tight whitespace-normal break-words">
-            I&apos;m a student of Pamantasan ng Lungsod ng Maynila
-          </p>
-        </button>
+            {/* Sub-text */}
+            <p className="text-sm text-center w-[244px] leading-tight whitespace-normal break-words">
+              I&apos;m a student of Pamantasan ng Lungsod ng Maynila
+            </p>
+          </button>
 
-        <button
-          onClick={handleUserTypeSelection.bind(null, "faculty")}
-          className="w-xs h-48 bg-accent border-2 rounded-lg hover:bg-white hover:shadow-lg transition-all duration-300 flex flex-col justify-center items-center p-6 cursor-pointer"
-        >
-          <FaChalkboardTeacher className="size-12 mb-2" />
+          <button
+            onClick={handleUserTypeSelection.bind(null, "faculty")}
+            className="w-xs h-48 bg-accent border-2 rounded-lg hover:bg-white hover:shadow-lg transition-all duration-300 flex flex-col justify-center items-center p-6 cursor-pointer"
+          >
+            <FaChalkboardTeacher className="size-12 mb-2" />
 
-          {/* Main heading */}
-          <p className="text-xl font-bold text-center text-gold leading-tight mb-1">
-            I&apos;m an educator
-          </p>
+            {/* Main heading */}
+            <p className="text-xl font-bold text-center text-gold leading-tight mb-1">
+              I&apos;m an educator
+            </p>
 
-          {/* Sub-text */}
-          <p className="text-sm text-center w-[244px] leading-tight whitespace-normal break-words">
-            I&apos;m a faculty member of Pamantasan ng Lungsod ng Maynila
-          </p>
-        </button>
+            {/* Sub-text */}
+            <p className="text-sm text-center w-[244px] leading-tight whitespace-normal break-words">
+              I&apos;m a faculty member of Pamantasan ng Lungsod ng Maynila
+            </p>
+          </button>
+        </div>
       </div>
+  
     </div>
   );
 };
