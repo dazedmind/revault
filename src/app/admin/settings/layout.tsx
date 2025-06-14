94debc562767d938ee22a54a364be5a0dbd48781
John Allen <dazedmind1@protonmail.com>
2025-06-15 00:58:59 +0800

    polishing and bug fixes

--- src/app/admin/settings/layout.tsx
+++ src/app/admin/settings/layout.tsx
@@ -61,7 +61,7 @@ export default function SettingsLayout({
                     </div>
                   </div>
                   <h1 className="text-3xl md:text-4xl font-bold mb-2">
-                    Librarian's System Settings
+                    Librarian&apos;s System Settings
                   </h1>
                 </div>
               </div>
