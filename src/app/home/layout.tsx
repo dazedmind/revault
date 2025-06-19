ce7884827df2f3bbafb19678769748d000aa24d8
John Allen <dazedmind1@protonmail.com>
2025-06-19 19:50:46 +0800

    added homescreen popup

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -15,6 +15,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
   const { theme } = useTheme();
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     // Add a small delay to prevent theme-related flashing
@@ -61,6 +62,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
   // Prevent theme changes from triggering auth loading
   useEffect(() => {
     if (authed) {
+      setShowPopup(true);
       setIsLoading(false);
     }
   }, [theme, authed]);
@@ -80,6 +82,27 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
         <NavBar />
       )}
       {children}
+
+      {showPopup && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center">
+          <div className="flex flex-col items-center justify-center text-center bg-accent p-6 w-lg mx-4 rounded-lg">
+            <p className="text-4xl">🎉</p>
+            <h2 className="text-2xl font-bold text-gold-fg">Welcome to PLM ReVault!</h2>
+            <p className="text-sm text-white-5 mb-4">
+              This is a beta testing version of the platform. Feel free to explore the platform and give us your feedback.
+
+            </p>
+            <a href="https://forms.gle/W5m4HZXtHiSXSjwG8" target="_blank" className="text-sm text-gold-fg mb-4 underline">
+              <p>Answer the survey here</p>
+            </a>
+            <button 
+             onClick={() => setShowPopup(false)}
+             className="bg-gold p-1 px-4 font-sans flex items-center gap-2 rounded-lg cursor-pointer hover:brightness-110 transition-all duration-200">
+              Let's go!
+            </button>
+          </div>
+        </div>
+      )}      
       <Toaster />
     </div>
   );
