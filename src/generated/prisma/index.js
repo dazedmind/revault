33b21dc5980d7f70d41a9b9d4523e925cdd70628
John Allen <dazedmind1@protonmail.com>
2025-06-21 13:33:09 +0800

    tailwind fix

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -274,7 +274,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
