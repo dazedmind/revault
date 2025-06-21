759b2b4a69b6aac331489da096caa8179b99b794
John Allen <dazedmind1@protonmail.com>
2025-06-21 13:18:58 +0800

    build fix 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
