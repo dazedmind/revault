de61bd28c34469b255f06af61238f2a25764400a
John Allen <dazedmind1@protonmail.com>
2025-06-21 14:49:54 +0800

    rever from yarn to npm

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Inter, IBM_Plex_Mono } from 'next/font/google'
+import '../lib/polyfill'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
